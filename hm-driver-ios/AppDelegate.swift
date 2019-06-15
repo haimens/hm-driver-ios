920d07d2ad13d5ad51aea38107b887a21d5fe74f
michaeldai1006 <dtc74110@gmail.com>
2019-06-14 17:38:39 -0700

    Init

--- hm-driver-ios/AppDelegate.swift
+++ hm-driver-ios/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  hm-driver-ios
-//
-//  Created by Michael Dai on 6/14/19.
-//  Copyright © 2019 haimens. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
--- hm-driver-ios/ViewController.swift
+++ hm-driver-ios/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  hm-driver-ios
-//
-//  Created by Michael Dai on 6/14/19.
-//  Copyright © 2019 haimens. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
