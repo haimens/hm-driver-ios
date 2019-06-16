109572f6f58bc697c2759cf6da358996c5b47225
michaeldai1006 <dtc74110@gmail.com>
2019-06-16 00:25:42 -0700

    Add spinner to HMLoginViewController

--- hm-driver-ios/Controllers/HMLogin/HMLoginViewController.swift
+++ hm-driver-ios/Controllers/HMLogin/HMLoginViewController.swift
@@ -1,17 +1,21 @@
 import UIKit
 
 class HMLoginViewController: UIViewController {
-    
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBAction func loginBtnClicked(_ sender: HMBasicButton) { makeLoginRequest() }
     
+    // UIElements
+    var spinner: TDSwiftSpinner!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Setup
         setupGestures()
         setupDelegates()
+        setupUI()
     }
     
     private func setupGestures() {
@@ -24,14 +28,28 @@ class HMLoginViewController: UIViewController {
         passwordTextField.delegate = self
     }
     
+    private func setupUI() {
+        spinner = TDSwiftSpinner(viewController: self)
+    }
+    
     private func makeLoginRequest() {
+        // Hide keyboard
+        self.view.endEditing(true)
+        
         // Username and password from textfield
         guard let username = usernameTextField.text, !username.isEmpty else { return }
         guard let password = passwordTextField.text, !password.isEmpty else { return }
         
+        // Show activity indicator
+        spinner.show()
+        
         // Perform login request
         TDSwiftHavana.shared.login(account: username, password: password) { (result, error) in
             DispatchQueue.main.async {
+                // Hide indicator
+                self.spinner.hide()
+                
+                // Handle login result
                 if result {
                     // Present main vc
                     self.performSegue(withIdentifier: String(describing: HMMainTabBarController.self), sender: self)
