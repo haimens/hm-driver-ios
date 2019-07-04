c83697e147a30298c8eeeedacb6b7feca877114f
michaeldai1006 <dtc74110@gmail.com>
2019-07-04 20:07:38 +0800

    logout

--- hm-driver-ios/Controllers/HMAccount/HMAccountViewController.swift
+++ hm-driver-ios/Controllers/HMAccount/HMAccountViewController.swift
@@ -11,4 +11,22 @@ class HMAccountViewController: UITableViewController {
         // Navigation appearance
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Deselect tableview cell
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath.row {
+        case 5: // Logout
+            // Remove current auth and user info
+            TDSwiftHavana.shared.removeAuthInfo()
+            TDSwiftHavana.shared.removeUserInfo()
+            
+            // Present auth vc
+            let authVC = self.storyboard!.instantiateViewController(withIdentifier: String(describing: HMAuthViewController.self))
+            self.present(authVC, animated: true, completion: nil)
+        default:
+            fatalError("ACCOUNT VC TABLEVIEW INDEX INVALID")
+        }
+    }
 }
