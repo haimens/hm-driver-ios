43efb04cf0d2f8c3562d0de27e70afea78430cf0
michaeldai1006 <dtc74110@gmail.com>
2019-07-10 19:00:09 +0800

    HMPersonalInfoViewController load data

--- hm-driver-ios/Controllers/HMPersonalInfo/HMPersonalInfoViewController.swift
+++ hm-driver-ios/Controllers/HMPersonalInfo/HMPersonalInfoViewController.swift
@@ -1,10 +1,16 @@
 import UIKit
 
 class HMPersonalInfoViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
         configNavigationAppearance()
+        loadData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -14,4 +20,19 @@ class HMPersonalInfoViewController: UIViewController {
     }
     
     private func configNavigationAppearance() { navigationController?.navigationBar.prefersLargeTitles = false }
+    
+    private func loadData() {
+        // App auth instance
+        guard let auth = TDSwiftHavana.shared.auth else {
+            TDSwiftAlert.showSingleButtonAlert(title: "Request Failed", message: "Driver info not found", actionBtnTitle: "OK", presentVC: self) {
+                self.navigationController?.popViewController(animated: true)
+            }
+            return
+        }
+        
+        // Display current driver info
+        nameTextField.text = auth.name
+        phoneTextField.text = auth.cell
+        emailTextField.text = auth.email
+    }
 }
