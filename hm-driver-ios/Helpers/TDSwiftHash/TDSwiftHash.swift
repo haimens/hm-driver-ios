423415c6cbd62e114744a8ca0fbd88fbab867aca
michaeldai1006 <dtc74110@gmail.com>
2019-07-14 11:48:05 +0800

    TDSwiftHash

---
+++ hm-driver-ios/Helpers/TDSwiftHash/TDSwiftHash.swift
@@ -0,0 +1,14 @@
+import Foundation
+import CommonCrypto
+
+public class TDSwiftHash {
+    public static func md5(_ string: String) -> String {
+        let data = Data(string.utf8)
+        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
+            return hash
+        }
+        return hash.map { String(format: "%02x", $0) }.joined()
+    }
+}
