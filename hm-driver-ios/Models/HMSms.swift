75325ce23eaa62695dee8d1cd923e42ef392e917
michaeldai1006 <dtc74110@gmail.com>
2019-07-25 00:32:31 +0800

    HMSms, sendSMSToDispatch

--- hm-driver-ios/Models/HMSms.swift
+++ hm-driver-ios/Models/HMSms.swift
@@ -7,6 +7,12 @@ class HMSms {
         }
     }
     
+    static func sendSMSToDispatch(withCustomerToken customerToken: String, body: [String:Any]?, completion: (([String: Any]?, Error?)->Void)?) {
+        DriverConn.request(method: "POST", endpoint: "/api/v0/sms/send/dispatch/\(customerToken)", query: nil, body: body, headers: nil) { (result, error) in
+            completion?(result, error)
+        }
+    }
+    
     static func getAllSMS(withCustomerToken customerToken: String, query: [String:Any]?, completion: (([String: Any]?, Error?)->Void)?) {
         DriverConn.request(method: "GET", endpoint: "/api/v0/sms/all/detail/customer/\(customerToken)", query: query, body: nil, headers: nil) { (result, error) in
             completion?(result, error)
