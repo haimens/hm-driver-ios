fe8086f05a0a324dc9a384c4ce1552592daa1fe2
michaeldai1006 <dtc74110@gmail.com>
2019-07-29 05:55:15 +0800

    TDSwiftIconCircleButton

---
+++ hm-driver-ios/UI/TDSwift/TDSwiftIconCircleButton/TDSwiftIconCircleButton.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+public class TDSwiftIconCircleButton: UIButton {
+    @IBInspectable
+    public var iconImage: UIImage = UIImage()
+    private var iconImageView: UIImageView!
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        updateAppearance()
+    }
+    
+    public func updateAppearance() {
+        // Shape layer
+        let roundedCornerMaskPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: self.frame.width / 2, height: self.frame.width / 2))
+        let shape = CAShapeLayer()
+        shape.path = roundedCornerMaskPath.cgPath
+        
+        // Shape and layer properties
+        shape.fillColor = self.backgroundColor?.cgColor
+        shape.frame = layer.bounds
+        layer.backgroundColor = UIColor.clear.cgColor
+        
+        // Apply
+        self.layer.insertSublayer(shape, at: 0)
+        
+        // Shadow
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowOpacity = 0.6
+        self.layer.shadowOffset = .zero
+        self.layer.shadowRadius = 5
+        
+        // Icon image view
+        iconImageView = UIImageView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 16.0, height: 16.0)))
+        iconImageView.image = iconImage
+        iconImageView.contentMode = .scaleAspectFit
+        iconImageView.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+        self.addSubview(iconImageView)
+    }
+}
